e132b8cf4ad10ac1b7034cbddfc3c65a017266bd
virtualglitch2009 <virtualglitch2009@gmail.com>
2025-03-24 18:17:44 +0000

    created signInWithCredential, needed implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:myapp/auth_gate.dart';
-
-import 'firebase_options.dart';
-
-// TODO(codelab user): Get API key
-const clientId = 
-"AIzaSyDgn2mxCTNTOalqsRtuwx5KWu0T6wV8sKM";
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
   runApp(const MyApp());
 }
 
@@ -22,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthGate(),
+      home: const HomeScreen(),
     );
   }
 }
