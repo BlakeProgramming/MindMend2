f5225fbcec1e02b99df4b79e2732fd6af15d91db
virtualglitch2009 <virtualglitch2009@gmail.com>
2025-05-15 18:38:07 +0000

    added deploy.sh

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:myapp/activity_center.dart';
+import 'package:myapp/sign_in_screen.dart';
 // import 'package:myapp/sudoku.dart';
 import 'firebase_options.dart';
 import 'gradient_theme.dart';
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       ),
-      home: ActivityCenter(),
+      home: SignInScreen(),
     );
   }
 }
